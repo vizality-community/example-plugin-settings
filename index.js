e53ae793fb95da633aaf4ee5def777d5ae018d94
dperolio <dustin.perolio@outlook.com>
2021-01-13 13:33:44 -0500

    Update to start / stop, remove settings, change settings to manifest key for testing

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
 import { Plugin } from '@vizality/entities';
 
-import Settings from './components/SettingsFunctional';
-
 export default class ExamplePluginSettings extends Plugin {
-  onStart () {
-    /*
-     * This won't be required later, as it will be automatically generated based
-     * on the existence of components/Settings.jsx
-     */
-    vizality.api.settings.registerAddonSettings({
-      id: this.addonId,
-      render: Settings
-    });
+  start () {
+    // *waves*
+  }
+
+  stop () {
+    // *waves*
   }
 }
